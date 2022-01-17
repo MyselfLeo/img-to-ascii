b9167b30b04ec19a48e90e2771663eecb71ffd64
MyselfLeo <myselfleo@outlook.fr>
2022-01-17 22:19:32 +0100

    removed

--- src/brightness_modulation.rs
+++
@@ -1,9 +0,0 @@
-// Brightness is an integer between 0 and 255.
-// The brightness of a pixel in an image is considered its "raw brightness".
-// The brightness_modulation functions will take the raw brightness and apply a brightness modulation.
-// The outputed brightness will still be between 0 and 255.
-
-
-fn linear_modulation(brightness: f64, mult: f64) -> f64 {
-    brightness * mult
-}
